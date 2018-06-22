2c0a0d6f7a6a548c37d099a943f553d6135735d6
dmiller22 <dmiller22kc@gmail.com>
2018-06-21 21:08:14 -0500

    Began parsing of basic player info

--- SoccerTeamAnalysis/SPI/Service/DataRetriever.cs
+++ SoccerTeamAnalysis/SPI/Service/DataRetriever.cs
@@ -8,7 +8,7 @@ namespace SPI.Service
 {
     public static class DataRetriever
     {        
-        public static string GetPlayerInfo(int LeagueID)
+        public static void GetPlayerInfo(int LeagueID)
         {
             WebClient client = new WebClient();
 
@@ -30,10 +30,71 @@ namespace SPI.Service
 
                 content = content.Substring(tbodyStart, tbodyEnd - tbodyStart);
 
+                pageNum++;
+
+                if (content.Length < 10) MorePlayers = false;
+
+                else
+                {
+                    ParsePlayerTable(content);
+                }
                 MorePlayers = false; //Temporary for debugging right now
             }
+        }
 
-            return "nichts";
+        //Dan - 06/21/2018 - Breaks up the table of multiple players into a list containing a string for each player's info
+        private static void ParsePlayerTable(string content)
+        {
+            List<string> players = new List<string>();
+
+            while (content.Length > 0)
+            {
+                int trStart = content.IndexOf("<tr>");
+                int trEnd = content.IndexOf("</tr>");
+
+                string playerInfo = content.Substring(trStart, trEnd - trStart);
+                players.Add(playerInfo);
+                content = content.Substring(trEnd + 6);
+            }
+
+            ExtractPlayerInfo(players);
+        }
+
+        //Dan - {COMPLETION DATE HERE} - Extracts all base player info from the table on the page read in
+        private static void ExtractPlayerInfo(List<string> players)
+        {
+            int PlayerID, OVR, POT;
+            string name, position;
+            string[] stringSeparators = new string[] { "<td" };
+            foreach (string player in players)
+            { 
+                string info = player.Replace("\n", string.Empty);
+                info = info.Replace("</td>", string.Empty);
+                string[] playerInfo = info.Split(stringSeparators, StringSplitOptions.None);
+                string startPath = playerInfo[1].Substring(playerInfo[1].IndexOf("href") + 6);
+                PlayerID = ParsePlayerID(startPath.Substring(0, startPath.IndexOf("\"")));
+
+                //Parse rest of info and store in db (once I get a db set up)
+            }
+        }
+
+        //Dan - 06/21/2018 - Parses the player ID from their link on the page
+        private static int ParsePlayerID(string path)
+        {
+            string reversed_path = Reverse(path);
+
+            string id_Reversed = reversed_path.Substring(0, reversed_path.IndexOf('/'));
+            int ID = Convert.ToInt32(Reverse(id_Reversed));
+
+            return ID;
+        }
+
+        //Dan - 06/21/2018 - Reverse a string (used to parse Player ID)
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
         }
     }
 
